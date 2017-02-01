9c98189dae5732eb4389d8fb964c200aa439461e
davegreeko <davegreeko1971@gmail.com>
2017-01-31 17:55:49 -0800

    added init.el
    Initial write

---
+++ init.el
@@ -0,0 +1,182 @@
+;; We don't want to type yes and no all the time so, do y and n
+(setq inhibit-startup-message t)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq auto-save-default nil)
+(tool-bar-mode -1)
+(global-linum-mode 1)
+(column-number-mode 1)
+(global-hl-line-mode 1)
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; You might already have this line
+(package-initialize)
+;; list the packages you want
+(defvar package-list)
+(setq package-list '(async auto-complete autopair clang-format cmake-ide
+                           cmake-mode company company-irony
+                           company-irony-c-headers dash epl flycheck
+                           flycheck-irony ivy swiper counsel projectile 
+                           google-c-style irony
+                           let-alist levenshtein magit pkg-info
+                           popup rtags seq solarized-theme web-mode
+                           window-numbering yasnippet))
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Require flycheck to be present
+(require 'flycheck)
+;; Force flycheck to always use c++11 support. We use
+;; the clang language backend so this is set to clang
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
+;; Turn flycheck on everywhere
+(global-flycheck-mode)
+
+
+;; Load rtags and start the cmake-ide-setup process
+(require 'rtags)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup cmake-ide
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'cmake-ide)
+(cmake-ide-setup)
+;; Set cmake-ide-flags-c++ to use C++11
+(setq cmake-ide-flags-c++ (append '("-std=c++11")))
+;; We want to be able to compile with a keyboard shortcut
+(global-set-key (kbd "C-c m") 'cmake-ide-compile)
+
+;; Set rtags to enable completions and use the standard keybindings.
+;; A list of the keybindings can be found at:
+;; http://syamajala.github.io/c-ide.html
+(setq rtags-autostart-diagnostics t)
+(rtags-diagnostics)
+(setq rtags-completions-enabled t)
+(rtags-enable-standard-keybindings)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package: yasnippet
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'yasnippet)
+;; To get a bunch of extra snippets that come in super handy see:
+;; https://github.com/AndreaCrotti/yasnippet-snippets
+;; or use:
+;; git clone https://github.com/AndreaCrotti/yasnippet-snippets.git ~/.emacs.d/yassnippet-snippets/
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippet-snippets/")
+(yas-global-mode 1)
+(yas-reload-all)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set up code completion with company and irony
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'company)
+(require 'company-rtags)
+(global-company-mode)
+
+;; Enable semantics mode for auto-completion
+(require 'cc-mode)
+(require 'semantic)
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+(semantic-mode 1)
+
+;; Setup irony-mode to load in c-modes
+(require 'irony)
+(require 'company-irony-c-headers)
+(require 'cl)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; irony-mode hook that is called when irony is triggered
+(defun my-irony-mode-hook ()
+  "Custom irony mode hook to remap keys."
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; company-irony setup, c-header completions
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+;; Remove company-semantic because it has higher precedance than company-clang
+;; Using RTags completion is also faster than semantic, it seems. Semantic
+;; also provides a bunch of technically irrelevant completions sometimes.
+;; All in all, RTags just seems to do a better job.
+(setq company-backends (delete 'company-semantic company-backends))
+;; Enable company-irony and several other useful auto-completion modes
+;; We don't use rtags since we've found that for large projects this can cause
+;; async timeouts. company-semantic (after company-clang!) works quite well
+;; but some knowledge some knowledge of when best to trigger is still necessary.
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers
+                        company-irony company-yasnippet
+                        company-clang company-rtags)
+    )
+  )
+
+(defun my-disable-semantic ()
+  "Disable the company-semantic backend."
+  (interactive)
+  (setq company-backends (delete '(company-irony-c-headers
+                                   company-irony company-yasnippet
+                                   company-clang company-rtags
+                                   company-semantic) company-backends))
+  (add-to-list
+   'company-backends '(company-irony-c-headers
+                       company-irony company-yasnippet
+                       company-clang company-rtags))
+  )
+(defun my-enable-semantic ()
+  "Enable the company-semantic backend."
+  (interactive)
+  (setq company-backends (delete '(company-irony-c-headers
+                                   company-irony company-yasnippet
+                                   company-clang) company-backends))
+  (add-to-list
+   'company-backends '(company-irony-c-headers
+                       company-irony company-yasnippet company-clang))
+  )
+
+;; Zero delay when pressing tab
+(setq company-idle-delay 0)
+(define-key c-mode-map [(C-tab)] 'company-complete)
+(define-key c++-mode-map [(C-tab)] 'company-complete)
+;; Delay when idle because I want to be able to think
+(setq company-idle-delay 0.2)
+
+;; Prohibit semantic from searching through system headers. We want
+;; company-clang to do that for us.
+(setq-mode-local c-mode semanticdb-find-default-throttle
+                 '(local project unloaded recursive))
+(setq-mode-local c++-mode semanticdb-find-default-throttle
+                 '(local project unloaded recursive))
+
+(semantic-remove-system-include "/usr/include/" 'c++-mode)
+(semantic-remove-system-include "/usr/local/include/" 'c++-mode)
+(add-hook 'semantic-init-hooks
+          'semantic-reset-system-include)
+
+
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (global-set-key "\C-s" 'swiper) 
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+
+(setq projectile-completion-system 'ivy)
+(projectile-mode 1)
